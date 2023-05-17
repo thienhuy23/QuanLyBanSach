728db007b86cb0fa6585b2e7c3467d6bb47f678b
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-05-18 06:22:06 +0700

    design home page

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,12 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.service.BookService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	BookService bookService;
+	
 	@GetMapping(value = {"","/","/home"})
-	public String home(){
+	public String home(Model model){
+		model.addAttribute("books",bookService.findAll());
 		return "page/home";
 	}
 	@GetMapping("/error")
