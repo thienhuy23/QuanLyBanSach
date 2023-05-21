eeea5717a90ef81ba3d0b7b347565171267812ca
Thienhuy23 <thienhuy2302003@gmail.com>
2023-05-21 15:59:19 +0700

    update profile

--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -2,18 +2,22 @@ package com.example.demo.controller;
 
 import java.util.Optional;
 
+import javax.swing.JOptionPane;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.Users;
 import com.example.demo.service.UsersService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/profile")
@@ -38,26 +42,16 @@ public class ProfileController {
 	}
 
 	@PostMapping("update")
-	public String updateProfile(@ModelAttribute("users") Users user, @RequestParam(name = "role") String role,
-			Model model) {
-		if (role != null) {
-			if (role.equals("1")) {
-				user.setRole(true);
-			} else {
-				user.setRole(false);
-			}
+	public String updateProfile(Model model, @Valid @ModelAttribute("users") Users user, BindingResult result) {
+		if (result.hasErrors()) {
+			model.addAttribute("message", "Một số trường không hợp lệ. Hãy sửa chúng!");
+		} else {
+			usersService.update(user);
+			model.addAttribute("users", user);
+			model.addAttribute("message", "Cập nhật thành công! !");
 		}
-
-		else {
-			user.setRole(false);
-		}
-		System.out.println("user update:" + user);
-		usersService.update(user);
-		model.addAttribute("users", new Users());
 
 		return "page/profile";
 	}
-
-
 
 }
