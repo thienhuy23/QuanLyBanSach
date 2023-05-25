0df5a003423965c9a1f0fb5950c21a2572c8fe4a
Thienhuy23 <thienhuy2302003@gmail.com>
2023-05-25 21:22:05 +0700

    update

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -2,7 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.dto.BookDto;
 
 @Controller
 @RequestMapping("/cart")
@@ -10,5 +16,12 @@ public class CartController {
 	@GetMapping(value={"/",""})
 	public String getRegister() {
 		return "page/cart";
+	}
+	
+	@ResponseBody
+	@PostMapping(value={"/",""})
+	public String getValue(@RequestBody BookDto book) {
+		System.out.println(book.toString());
+		return "ok";
 	}
 }
