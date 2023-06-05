9d4acf8b313aeba5e64f20702c961e12b7801e48
Thienhuy23 <thienhuy2302003@gmail.com>
2023-06-05 11:49:35 +0700

    update quan ly tk ,loai sp

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -25,41 +25,53 @@ public class AdminController {
 	UsersService usersService;
 	@Autowired
 	CategoryService categoryService;
+
 	@RequestMapping("/admin")
 	public String home() {
 		return "page/statistical_admin";
 	}
+
 	@RequestMapping("/supplier")
 	public String supplier() {
 		return "page/supplier_admin";
 	}
+
 	@RequestMapping("/author")
 	public String author() {
 		return "page/author_admin";
 	}
+
 	@RequestMapping("/category")
-	public String category() {
+	public String category(Model model) {
+		List<Category> category = categoryService.findAll();
+		model.addAttribute("categorys", category);
 		return "page/category_admin";
 	}
+
 	@RequestMapping("/image")
 	public String image() {
 		return "page/image_admin";
 	}
+
 	@RequestMapping("/product")
 	public String product() {
 		return "page/book_admin";
 	}
+
 	@RequestMapping("/account")
-	public String account() {
+	public String account(Model model) {
+		List<Users> user = usersService.findAll();
+		model.addAttribute("users", user);
 		return "page/account_admin";
 
 	}
+
 	@RequestMapping("/order")
 	public String order() {
 		return "page/order_admin";
 
 	}
-	
+
 	@RequestMapping("/admin_js")
 	public String home(Model model) {
 		List<Users> user = usersService.findAll();
@@ -67,53 +79,57 @@ public class AdminController {
 		List<Category> category = categoryService.findAll();
 		model.addAttribute("categorys", category);
 		return "page/admin";
+
 	}
-	
+
 	@PostMapping("/user/create")
-	public String createUser(Model model, @ModelAttribute("users") Users user) {	
-			System.out.println("user update:" + user);
-			usersService.save(user);
-			model.addAttribute("users", user);
-			model.addAttribute("message", "Thêm mới thành công!");
-		return "redirect:/admin";
+	public String createUser(Model model, @ModelAttribute("users") Users user) {
+		System.out.println("user update:" + user);
+		usersService.save(user);
+		model.addAttribute("users", user);
+		model.addAttribute("message", "Thêm mới thành công!");
+		return "redirect:/account";
 	}
-	
+
 	@PostMapping("/user/update")
-	public String updateUser(Model model, @ModelAttribute("users") Users user) {	
-			System.out.println("user update:" + user);
-			usersService.update(user);
-			model.addAttribute("users", user);
-			model.addAttribute("message", "Cập nhật thành công!");
-		return "redirect:/admin";
+	public String updateUser(Model model, @ModelAttribute("users") Users user) {
+		System.out.println("user update:" + user);
+		usersService.update(user);
+		model.addAttribute("users", user);
+		model.addAttribute("message", "Cập nhật thành công!");
+		return "redirect:/account";
 	}
-	
+
 	@RequestMapping("/user/delete/{id}")
-	public String DeletetUsersId( @PathVariable("id") int id) {		
+	public String DeletetUsersId(@PathVariable("id") int id, Model model) {
 		usersService.deleteUserId(id);
-		return "/page/admin";
+		List<Users> user = usersService.findAll();
+		model.addAttribute("users", user);
+		return "redirect:/account";
 	}
-	
+
 	@PostMapping("/category/create")
-	public String createCategory(Model model, @ModelAttribute("category") Category category) {	
-			System.out.println("user update:" + category);
-			categoryService.createCategory(category);
-			model.addAttribute("users", category);
-			model.addAttribute("message", "Thêm mới thành công!");
-		return "page/admin";
+	public String createCategory(Model model, @ModelAttribute("category") Category category) {
+		System.out.println("user update:" + category);
+		categoryService.createCategory(category);
+		model.addAttribute("users", category);
+		model.addAttribute("message", "Thêm mới thành công!");
+		return "redirect:/category";
 	}
+
 	@PostMapping("/category/update")
-	public String updateCategory(Model model, @ModelAttribute("category") Category category) {	
-			System.out.println("user update:" + category);
-			categoryService.update(category);
-			model.addAttribute("users", category);
-			model.addAttribute("message", "Cập nhật thành công!");
-		return "redirect:/admin";
+	public String updateCategory(Model model, @ModelAttribute("category") Category category) {
+		System.out.println("user update:" + category);
+		categoryService.update(category);
+		model.addAttribute("users", category);
+		model.addAttribute("message", "Cập nhật thành công!");
+		return "redirect:/category";
 	}
-	
+
 	@RequestMapping("/category/delete/{id}")
-	public String DeleteCategoryId(Model model, @PathVariable("id") int id) {		
-		categoryService.deleteCategoryId(id);	
-		return "/page/admin";
+	public String DeleteCategoryId(Model model, @PathVariable("id") int id) {
+		categoryService.deleteCategoryId(id);
+		return "redirect:/category";
 	}
-	
+
 }
