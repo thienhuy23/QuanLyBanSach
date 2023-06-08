5292427d5cf93c0c95476d06d9800959a938049e
Thienhuy23 <thienhuy2302003@gmail.com>
2023-06-09 00:14:03 +0700

    update image, quan ly tk

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -12,13 +12,16 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Author;
+import com.example.demo.entity.Book;
 import com.example.demo.entity.Category;
 import com.example.demo.entity.Image;
 import com.example.demo.entity.Supplier;
@@ -28,6 +31,7 @@ import com.example.demo.repository.CategoryRepository;
 import com.example.demo.repository.SupplierRepository;
 import com.example.demo.repository.UsersRepository;
 import com.example.demo.service.AuthorService;
+import com.example.demo.service.BookService;
 import com.example.demo.service.CategoryService;
 import com.example.demo.service.ImageService;
 import com.example.demo.service.SupplierService;
@@ -58,6 +62,8 @@ public class AdminController {
 	AuthorRepository AuthorRep;
 	@Autowired
 	ImageService imgservice;
+	@Autowired
+	BookService bookservice;
 
 	@RequestMapping("/admin")
 	public String home(HttpSession session, Principal principal) {
@@ -167,7 +173,45 @@ public class AdminController {
 	@RequestMapping("/image")
 	public String image(Model model) {
 		List<Image> img = imgservice.findAll();
+		model.addAttribute("book", bookservice.findAll());
 		model.addAttribute("img", img);
+		return "page/image_admin";
+	}
+	@PostMapping("/image")
+	public String handleImage(Model model, @RequestParam("url") String url,
+							  @RequestParam(value = "id", required = false) String idString,
+							  @RequestParam("bookId") int bookId,
+							  @RequestParam(value = "action", required = false) String action) {
+		int id = 0; // Default value for id
+		if (idString != null && !idString.isEmpty()) {
+			id = Integer.parseInt(idString);
+		}
+		if (action != null && action.equals("add")) {
+		
+			Image img = new Image();
+			Optional<Book> book = bookservice.findById(bookId);
+			img.setBook(book.get());
+			img.setUrl(url);
+			imgservice.save(img);
+		}else if(action != null && action.equals("edit")){
+			Optional<Image> image = imgservice.findById(id);
+			if(image.isPresent()){
+				image.get().setUrl(url);
+				image.get().setBook(bookservice.findById(bookId).get());
+				imgservice.save(image.get());
+			}
+		}else if (action.equals("delete")) {
+		
+		}
+		return "redirect:/image";
+	}
+
+	@GetMapping("/image/edit")
+	public String editImage(Model model, @RequestParam("id") int id) {
+		Optional<Image> img = imgservice.findById(id);
+		model.addAttribute("book", bookservice.findAll());
+		model.addAttribute("image", img.get());
+		model.addAttribute("img", imgservice.findAll());
 		return "page/image_admin";
 	}
 
@@ -218,16 +262,12 @@ public class AdminController {
 		return "redirect:/account";
 	}
 
-	@RequestMapping("/user/edit/{id}")
-	public String editUser(Model model, @PathVariable("id") int id) {
-//		Users user = userrepo.findById(id)
-//			      .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-//			    
-//			    model.addAttribute("users", user);
+	@RequestMapping(value = "/user/edit", method = RequestMethod.GET)
+	public String editUser(Model model, @RequestParam("id") int id) {
 		Optional<Users> user = usersService.findById(id);
-		model.addAttribute("users", user.get());
-		System.out.println(user);
-		return "redirect:/account";
+		model.addAttribute("users", usersService.findAll());
+		model.addAttribute("user", user.orElse(new Users()));
+		return "page/account_admin";
 	}
 
 	@RequestMapping("/user/delete/{id}")
--- src/main/java/com/example/demo/service/ImageService.java
+++ src/main/java/com/example/demo/service/ImageService.java
@@ -1,22 +1,24 @@
 package com.example.demo.service;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Image;
-
 import com.example.demo.repository.ImageRepository;
-
 @Service
 public class ImageService {
 	@Autowired
 	ImageRepository imgrepo;
-	
 	public List<Image> findAll() {
 		return imgrepo.findAll();
 	}
-
+	public Optional<Image> findById(int id) {
+		return imgrepo.findById(id);
+	}
+	public Image save(Image img) {
+		return imgrepo.save(img);
+	}
 }
